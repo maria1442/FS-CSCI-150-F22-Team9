06891aed4e6e1dcf16ba9f0264d1555d67497649
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-11-01 15:07:04 -0700

    Updated index.js
    Added some encryption functionalities but still working on the environment variables

--- students/index.js
+++ students/index.js
@@ -1,7 +1,29 @@
+if(process.env.NODE_ENV !== 'prodcution'){
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app=express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const initializePassport = require('./passport-config');
+const flash = require('express-flash');
+const session = require('express-session');
+
+initializePassport(
+    passport, 
+    email => {
+    return users.find(users => users.email === email)
+});
+
+const users = [];
 
 app.set('view-engine','ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(flash());
+app.use(session(
+    secret: process.env.SESSION_SECRET
+));
 
 app.get("/", function(req,res){
     res.render('index.ejs', {name: "Aakash"});
@@ -11,8 +33,33 @@ app.get("/login", function(req,res){
     res.render('login.ejs');
 })
 
-app.get('/register', function(req, res){
-    res.render("register.ejs");
+// post for log in
+app.post('/login', function(req,res){
+
+})
+
+// get for register
+
+app.get('/register',function(req, res){
+    res.render('register.ejs');
+})
+
+app.post('/register',async function(req, res){
+    try{
+        const hashedPassword = await (bcrypt.hash(req.body.password, 10));
+        users.push({
+            id: Date.now().toString,
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        res.redirect('/login');
+        
+    }
+    catch{
+        res.redirect('/register');
+    }
+    console.log(users);
 })
 
 app.listen(3000, function(){
